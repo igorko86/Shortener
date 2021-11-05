7010db2c5dd7127c2c7b7194b8457181553e7360
Igor <mocherad86@gmail.com>
2021-11-05 16:46:58 +0200

    added global styles

---
+++ client/src/components/ListArea/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const DivListArea = styled.div`
+  max-width: 300px;
+  border-radius: 3px;
+  border: 1px solid red;
+  max-height: 100%;
+  padding: 0 4px;
+  background: ${({ theme }) => theme.colors.brightGray};
+`;
---
+++ client/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'secondary',
+    brightGray: '#ebecf0',
+  },
+};
+
+export { theme };
