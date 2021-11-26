0aaf5f1efc454a70e6c567392a143a9ae014b3ea
YLiubchenko <luibchenko0606@gmail.com>
2021-11-27 00:08:31 +0200

    default styled

--- client/src/styled.d.ts
+++ client/src/styled.d.ts
@@ -3,11 +3,25 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     borderRadius: string;
+    boxShadow: string;
+    marginTop: string;
 
     colors: {
       main: string;
       secondary: string;
       brightGray: string;
+      midnight: string;
+      lightGray: string;
+      lightGray1: string;
+      silver: string;
+      mainColor: string;
+      snow: string;
+      gainsboro: string;
+      suvaGrey: string;
+      iron: string;
+      whiteSmoke: string;
+      pinkSwan: string;
+      kashmirBlue: string;
     };
   }
 }
